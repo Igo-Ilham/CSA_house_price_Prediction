e0ca2c980a51ab90ced1c5d1ad8fb5e4e41d7170
igo-ilham <Mirainee3@gmail.com>
2023-12-30 15:25:46 +0700

    update layout

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -6,11 +6,10 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
-
 # making sidebar
 with st.sidebar:
     selected = option_menu("Main Menu",
-    ["About Project", "Analysis",
+    ["About Project", "Analytics",
      "Prediction"],
     default_index=0)
 
@@ -48,58 +47,63 @@ if (selected == "About Project"):
     '''
     st.markdown(f'<div style="text-align: justify;">{multi_line7}</div>', unsafe_allow_html=True)
     
+if (selected == "Analytics"):
+    tab1, tab2 = st.tabs(["Introduction","Visualization"])
+    
+    with tab1:
+        st.header("Exploratory Data Analysis (EDA)")
+        
+        st.subheader('**Pendahuluan Umum :**')
+        multi_line1 = '''"Dalam rangka menghadirkan analisis data yang komprehensif, kita akan memulai dengan menjelajahi dataset yang telah dikumpulkan. 
+        Dataset ini merupakan kumpulan data yang relevan dengan tujuan proyek data science kita."'''
+        st.markdown(f'<div style="text-align: justify;">{multi_line1}</div>', unsafe_allow_html=True)
+        
+        st.subheader('**Deskripsi Dataset :**')
+        multi_line2 = '''Sebelum kita memasuki tahap analisis, mari kita terlebih dahulu memahami struktur dan karakteristik dari dataset yang akan kita gunakan.
+        Dataset ini mencakup informasi seputar data ukuruan luas tanah, luas bangunan, jumlah kamar tidur, jumlah kamar mandi & garasi yang diharapkan dapat memberikan inshigt untuk melakukan pembuatan model prediksi."'''
+        st.markdown(f'<div style="text-align: justify;">{multi_line2}</div>', unsafe_allow_html=True)
+        
+        st.subheader('**Sumber Data :**')
+        multi_line3 = '''""Dataset yang digunakan dalam proyek ini diperoleh dari situs Kaggle.com pada link = https://www.kaggle.com/datasets/wisnuanggara/daftar-harga-rumah. 
+        Keterpahaman terhadap sumber data sangat penting untuk memastikan validitas dan relevansi analisis yang akan kita lakukan."'''
+        st.markdown(f'<div style="text-align: justify;">{multi_line3}</div>', unsafe_allow_html=True)
+        
+        st.text("")
+        st.caption("**Dataset Harga Rumah :**")
+        # import dataset
+        data = pd.read_csv("Harga_Rumah.csv")
+        st.dataframe(data, width=800)
+        
+        st.write("Penjelasan keterangan setiap kolom :")
+        st.markdown("- LB = Luas Bangunan")
+        st.markdown("- LT = Luas Tanah")
+        st.markdown("- KT = Kamar Tidur")
+        st.markdown("- KM = Kamar Mandi")
+        st.markdown("- GRS = Garasi")
+        
+        st.markdown('''
+        <style>
+        [data-testid="stMarkdownContainer"] ul{
+            padding-left:20px;
+        }
+        </style>
+        ''', unsafe_allow_html=True)
 
-if (selected == "Analysis"):
-    st.header("Exploratory Data Analysis (EDA)")
-    
-    st.subheader('**1. Pendahuluan Umum :**')
-    multi_line1 = '''"Dalam rangka menghadirkan analisis data yang komprehensif, kita akan memulai dengan menjelajahi dataset yang telah dikumpulkan. 
-    Dataset ini merupakan kumpulan data yang relevan dengan tujuan proyek data science kita."'''
-    st.markdown(f'<div style="text-align: justify;">{multi_line1}</div>', unsafe_allow_html=True)
-    
-    st.subheader('**2. Deskripsi Dataset :**')
-    multi_line2 = '''Sebelum kita memasuki tahap analisis, mari kita terlebih dahulu memahami struktur dan karakteristik dari dataset yang akan kita gunakan.
-    Dataset ini mencakup informasi seputar data ukuruan luas tanah, luas bangunan, jumlah kamar tidur, jumlah kamar mandi & garasi yang diharapkan dapat memberikan inshigt untuk melakukan pembuatan model prediksi."'''
-    st.markdown(f'<div style="text-align: justify;">{multi_line2}</div>', unsafe_allow_html=True)
-    
-    st.subheader('**3. Tujuan Analisis :**')
-    multi_line3 = '''"Analisis data yang efektif dimulai dengan pemahaman yang mendalam terhadap dataset. Dalam konteks ini, 
-    kita akan menyelidiki dataset ini untuk melakukan suatu prediksi terhadap harga sebuah rumah dengan spesifikasi tertentu."'''
-    st.markdown(f'<div style="text-align: justify;">{multi_line3}</div>', unsafe_allow_html=True)
-
-    st.subheader('**4. Sumber Data :**')
-    multi_line4 = '''""Dataset yang digunakan dalam proyek ini diperoleh dari situs Kaggle.com pada link = https://www.kaggle.com/datasets/wisnuanggara/daftar-harga-rumah. 
-    Keterpahaman terhadap sumber data sangat penting untuk memastikan validitas dan relevansi analisis yang akan kita lakukan."'''
-    st.markdown(f'<div style="text-align: justify;">{multi_line4}</div>', unsafe_allow_html=True)
-    
-    st.text("")
-    st.caption("**Dataset Harga Rumah :**")
-    # import dataset
-    data = pd.read_csv("Harga_Rumah.csv")
-    st.dataframe(data, width=800)
-    
-    st.write("Penjelasan keterangan setiap kolom :")
-    st.markdown("- LB = Luas Bangunan")
-    st.markdown("- LT = Luas Tanah")
-    st.markdown("- KT = Kamar Tidur")
-    st.markdown("- KM = Kamar Mandi")
-    st.markdown("- GRS = Garasi")
-    
-    st.markdown('''
-    <style>
-    [data-testid="stMarkdownContainer"] ul{
-        padding-left:20px;
-    }
-    </style>
-    ''', unsafe_allow_html=True)
-
-    st.markdown("***Melakukan perbandingan harga dengan beberapa atribut feature***")
-    img = Image.open("Resources/perbandingan.png")
-    st.image(img, caption='visualisasi data dengan regplot')
-    
-    st.markdown("***Melakukan Analisa Univariat terhadap variabel luas tanah***")
-    img = Image.open("Resources/LT_analisis_univariat.png")
-    st.image(img, caption='visualisasi dengan kde dan boxplot')
+        st.subheader('**Tujuan Analisis :**')
+        multi_line4 = '''"Analisis data yang efektif dimulai dengan pemahaman yang mendalam terhadap dataset. Dalam konteks ini, 
+        kita akan menyelidiki dataset ini untuk melakukan suatu prediksi terhadap harga sebuah rumah dengan spesifikasi tertentu."'''
+        st.markdown(f'<div style="text-align: justify;">{multi_line4}</div>', unsafe_allow_html=True)
+        
+    with tab2:
+        col3, col4 = st.columns(2)
+        
+        st.markdown("***Melakukan perbandingan harga dengan beberapa atribut feature***")
+        img = Image.open("Resources/perbandingan.png")
+        st.image(img, caption='visualisasi data dengan regplot')
+                
+        st.markdown("***Melakukan Analisa Univariat terhadap variabel luas tanah***")
+        img = Image.open("Resources/LT_analisis_univariat.png")
+        st.image(img, caption='visualisasi dengan kde dan boxplot')
     
 if (selected == "Prediction"):
     
